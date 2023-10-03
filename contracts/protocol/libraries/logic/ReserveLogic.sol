f23e348d0b0df5ec41971b8dd884e181b542a0d0
Sanghyun Yi <shyi0211@gmail.com>
2023-10-03 21:18:24 +0900

    feat: create math utils

--- contracts/protocol/libraries/logic/ReserveLogic.sol
+++ contracts/protocol/libraries/logic/ReserveLogic.sol
@@ -2,3 +2,12 @@
 pragma solidity 0.8.10;
 
 import {ReserveConfiguration} from '../configuration/ReserveConfiguration.sol';
+import {MathUtils} from '../math/MathUtils.sol';
+import {WadRayMath} from '../math/WadRayMath.sol';
+import {PercentageMath} from '../math/PercentageMath.sol';
+import {Errors} from '../helpers/Errors.sol';
+import {DataTypes} from '../types/DataTypes.sol';
+
+library ReserveLogic {
+  
+}
